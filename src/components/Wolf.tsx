415c142eacab1c10c809835eeb7c97bb0aee8b95
kevindmorris <kdm0030@mix.wvu.edu>
2024-04-03 10:56:10 -0400

    Add basic animation

--- src/components/Wolf.tsx
+++ src/components/Wolf.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useAnimate } from "framer-motion";
 
 export default function Wolf() {
+  const [scope, animate] = useAnimate();
+
+  React.useEffect(() => {
+    (async () => {
+      await animate(
+        scope.current,
+        { opacity: 1, rotate: 360, scale: 1 },
+        { duration: 1, delay: 1 }
+      );
+      await animate(
+        scope.current,
+        { x: -100, rotate: -360 },
+        { duration: 1.5 }
+      );
+      await animate(scope.current, { y: -100 }, {});
+      await animate(scope.current, { x: 0, rotate: 0 }, { duration: 1.5 });
+      await animate(scope.current, { y: 0 }, {});
+    })();
+  }, []);
+
   return (
-    <motion.h1
-      initial={{ opacity: 0, scale: 0, rotate: "720deg" }}
-      animate={{ opacity: 1, scale: 1, rotate: "0deg" }}
-      transition={{ duration: 5, type: "spring" }}
-      style={{ fontSize: "5rem" }}
-    >
+    <motion.h1 ref={scope} initial={{ opacity: 0, scale: 0 }}>
       🐺
     </motion.h1>
   );
